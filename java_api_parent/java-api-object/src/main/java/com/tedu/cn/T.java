ca8b3c63711406142f708139733ea0c17d5bacbc
zhouxiaowei2019 <2603843429@qq.com>
2019-11-24 16:56:01 +0800

    提交

---
+++ java_api_parent/java-api-object/src/main/java/com/tedu/cn/T.java
@@ -0,0 +1,11 @@
+package com.tedu.cn;
+
+public class T {
+    public static void main(String[] args) {
+
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈");
+    }
+}
