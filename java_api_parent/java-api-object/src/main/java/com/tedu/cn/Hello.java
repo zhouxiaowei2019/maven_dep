b83a11b900692858368759915c8d5a5c322f379e
zhouxiaowei2019 <2603843429@qq.com>
2019-11-24 16:46:54 +0800

    提交

--- java_api_parent/java-api-object/src/main/java/com/tedu/cn/Hello.java
+++ java_api_parent/java-api-object/src/main/java/com/tedu/cn/Hello.java
@@ -3,5 +3,9 @@ package com.tedu.cn;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
+        System.out.println("*******");
     }
 }
